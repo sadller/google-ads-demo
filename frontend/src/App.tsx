8d112eb3b9244fb60082919ef60e385f8410a780
sumit pandey <sumit.pandey@optym.com>
2025-12-14 11:30:36 +0530

    frontend base setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Pathik</h1>
+      <p>Welcome to your application</p>
+    </div>
+  )
+}
+
+export default App
